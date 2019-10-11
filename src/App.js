d8327b0bee782032c5a079f734c95f88463d4b1d
danielospina@gmail.com <danielospina@gmail.com>
2019-10-11 18:49:35 -0500

    tareaterminadas

--- src/App.js
+++ src/App.js
@@ -13,17 +13,67 @@ class App extends Component {
       ],
       newTask: ''
     }
+    this.updateTarea = this.updateTarea.bind(this);
+          this.keyPress = this.keyPress.bind(this);
+      };
+
+      updateTarea(event) {
+    this.setState({newTask: event.target.value});
+    }
+
+    keyPress(event){
+      event.preventDefault();
+    if (this.state.newTask) {
+
+      this.setState({
+        tasks: this.state.tasks.concat([{
+          id: this.state.tasks.length+1,
+          name: this.state.newTask,
+          done: false
+        }]),
+        newTask:""
+      })
+
+    }
+        //  event.key === 'Enter' ? event.preventDefault(): console.log("no")
+             // put the login here
+
+          }
+
+tachar(id){
+  const newTask = this.state.tasks.filter(n =>{
+    if (n.id === id) {
+      n.done=!n.done
+    }
+    return n
+  });
+  this.setState({
+    tasks: newTask
+  })
   }
+
+
+
+
+
+
   render() {
+
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li className={task.done?"done":''} key={task.id} onClick={()=>this.tachar(task.id)}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.keyPress}>
+            <input
+            type="text" id="new-task"
+            placeholder="Ingresa una tarea y oprime Enter"
+            value={this.state.newTask}
+            onChange={this.updateTarea}
+            className={this.state.newTask?"":"error"}
+            />
           </form>
         </div>
       </div>
